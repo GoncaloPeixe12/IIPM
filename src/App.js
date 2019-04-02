abd3f2a98e9f1704a37f8d1b84fbc7d22044a2d2
Sparkybeard <goncalo@bash.pt>
2019-04-02 17:32:55 +0100

    first Send

--- src/App.js
+++ src/App.js
@@ -1,27 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Carousel from 'react-bootstrap/Carousel'
+import Container from 'react-bootstrap/Container'
+import Col from 'react-bootstrap/Col'
+import 'bootstrap/dist/css/bootstrap.css';
+import { FaHome, FaAngleDoubleLeft } from "react-icons/fa";
+import Row from "react-bootstrap/Row";
+
 
 class App extends Component {
   render() {
+    const divStyle = {
+      backgroundColor: 'black',
+      height: '40px',
+    };
+    const divStyle1 = {
+      backgroundColor: 'black',
+    };
+
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+        <div>
+          <div className='container-fluid home'>
+            <div className='row'>
+              <div className='col-1'> <FaHome/> </div>
+              <div className='col-1'> <FaAngleDoubleLeft/> </div>
+              <div className='col barbecue'>  </div>
+            </div>
+          </div>
+          <Hub/>
+          <Container>
+            <Row>
+              <Col style={divStyle}>
+                hey
+              </Col>
+              <Col style={divStyle1}>
+                oi
+              </Col>
+              <Col>
+                lul
+              </Col>
+            </Row>
+          </Container>
+            <StreamFuncionality/>
+        </div>
     );
+  }
+}
+
+class Hub extends Component {
+  render() {
+    return(
+        <div className={'ponto1'}>
+          <Carousel interval={null} indicators={false}>
+            <Carousel.Item>
+              <div > lelitos</div>
+            </Carousel.Item>
+            <Carousel.Item>
+              <div > lolitos</div>
+            </Carousel.Item>
+          </Carousel>
+        </div>
+    )
+  }
+}
+class StreamFuncionality extends Component {
+  render() {
+    return(
+        <div className={'funcionalidade'}>
+          oi
+        </div>
+    )
   }
 }
 
