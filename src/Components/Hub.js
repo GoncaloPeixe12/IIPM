89335ad9c81bab0ccd0ee560a1140be4b8129b37
Sparkybeard <goncalo@bash.pt>
2019-04-03 11:23:13 +0100

    Finished skeleton

--- src/Components/Hub.js
+++ src/Components/Hub.js
@@ -1,14 +1,19 @@
 import Carousel from 'react-bootstrap/Carousel';
 import React, {Component} from 'react';
 import {FaRegSun, FaRunning, FaCamera} from 'react-icons/fa';
+import Link from 'react-bootstrap/Link';
 
 class Hub extends Component {
     render() {
+        const style = {
+            backgroundColor: '#3a79e0',
+        };
+
         return(
-            <div className={'ponto1'}>
+            <div className={'ponto1'} style={style}>
                 <Carousel interval={null} indicators={false}>
                     <Carousel.Item>
-                        <div ><FaRegSun size='5em'/></div>
+                        <div><Link href={}><FaRegSun size='5em'/></Link></div>
                     </Carousel.Item>
                     <Carousel.Item>
                         <div ><FaRunning size='5em'/></div>
