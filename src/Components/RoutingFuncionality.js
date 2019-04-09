c72cc2fe43e85a822dd2d5a8ffdbb018507d7e69
Sparkybeard <goncalo@bash.pt>
2019-04-09 19:34:56 +0100

    links on route

--- src/Components/RoutingFuncionality.js
+++ src/Components/RoutingFuncionality.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container'
 import Col from 'react-bootstrap/Col'
 import Row from "react-bootstrap/Row";
 import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
 
 class RoutingFuncionality extends Component{
     render() {
@@ -22,15 +23,19 @@ class RoutingFuncionality extends Component{
                 <Container style={containerStyle}>
                     <Row>
                         <Col style={style}>
-                            <Button variant={'dark'} size={'lg'}>Plan</Button>
+                            <Button variant={'dark'} size={'lg'}><Link to={'FuncRouter/1/1'}>Plan</Link></Button>
                         </Col>
                         <Col style={style}>
-                            <Button variant={'dark'} size={'lg'}>Start Walking!</Button>
+                            <Button variant={'dark'} size={'lg'}>
+                                <Link to={'FuncRouter/1/2'}>Start Walking!</Link>
+                            </Button>
                         </Col>
                     </Row>
                     <Row>
                         <Col style={style}>
-                            <Button variant={'dark'} size={'lg'}>:.:</Button>
+                            <Button variant={'dark'} size={'lg'}>
+                                <Link to={'FuncRouter/1/3'}>:.:</Link>
+                            </Button>
                         </Col>
                     </Row>
                 </Container>
