12f20f9b8d9e11d06e5f72b21da3286fa10416eb
Duarte Matias <dsm@localhost.localdomain>
2019-04-10 02:40:17 +0100

    Finished Settings CSS

--- src/Components/MainOptions.js
+++ src/Components/MainOptions.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
 import '../style/Home.css';
 import '../style/MainOptions.css';
+import {Link, } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import { FaHome, FaAngleDoubleLeft } from "react-icons/fa";
+import Button from 'react-bootstrap/Button';
 
 
 class MainOptions extends Component {
@@ -14,6 +19,7 @@ class MainOptions extends Component {
                     <div className={"opt"}>About</div>
                 </div>
                 <div className={'options'}>
+                    <Button style = {{border:'solid 2px black'}}onClick={this.props.history.goBack}><FaAngleDoubleLeft/></Button>
                     <div className={'tabItem'}>
                     </div>
                     <div className={'tabItem'}>
