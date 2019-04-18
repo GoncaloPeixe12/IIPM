f6497a2b79303ee3ce0d68ccecc68b2c1f9bc09c
Duarte <32573496+dsm43@users.noreply.github.com>
2019-04-18 20:29:52 +0100

    Finished Stream Screen, fixed Redirect and Back

---
+++ src/Components/Streaming/StreamFunc.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import {FaStop, FaPause, FaPlay, FaCog } from 'react-icons/fa';
+import '../../style/Streaming/StreamFunc.css';
+import {Redirect} from 'react-router-dom';
+
+class StreamFunc extends Component{
+    state = {
+        paused: false,
+        toMenu: false,
+        toSettings: false,
+    }
+
+    handleClick(evt){
+        if(this.state.paused === false){
+            this.setState(() => ({
+                paused: true,
+            }))
+        }
+        else(
+            this.setState(() => ({
+                paused: false,
+            }))
+        )
+    }
+
+    handleMenu = (user) => {
+        this.setState(() => ({
+            toMenu: true,
+        }))
+    }
+
+    handleSettings = (user) => {
+        this.setState(() => ({
+            toSettings: true,
+        }))
+    }
+
+    render(){
+        
+        if(this.state.toMenu === true){
+            return(<Redirect push to='/StreamFunc'/>)
+        }
+
+        if(this.state.toSettings === true){
+            return (<Redirect push to='/StreamFunc/1/1'/>)
+        }
+
+        const visible = {
+            display: 'inline-block',
+        };
+
+        const invisible = {
+            display: 'none',
+        };
+
+
+        let play = this.state.paused ? invisible : visible;
+        let pause = this.state.paused ? visible : invisible;
+        
+
+
+        return (
+            <div className={'buttonContainer streamButtonCont'}>
+                <button className={'streamButton'} onClick={()=>this.handleClick()}><FaPlay style = {play} size='4em'/><FaPause style={pause} size='4em'/></button>
+                <button className={'streamButton'} onClick={()=>this.handleMenu()}><FaStop size='4em'/></button>  
+                <button className={'streamButton'} onClick={()=>this.handleSettings()}><FaCog size='4em'/></button>  
+            </div>
+        )
+    }
+}
+export default StreamFunc;
--- src/Components/Streaming/StreamMenu.js
+++ src/Components/Streaming/StreamMenu.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import '../../style/Streaming/StreamMenu.css';
-import {FaPhone, FaVideo, FaCog, FaAngleDoubleLeft } from 'react-icons/fa';
+import {FaPhone, FaVideo, FaCog, FaHome } from 'react-icons/fa';
 import { Redirect  } from 'react-router-dom';
 import '../../style/Streaming/Menu.css';
 import '../../style/Home.css';
@@ -9,6 +9,7 @@ class StreamMenu extends Component{
     state = {
         toStream : false,
         toCall : false,
+        toHome: false,
     }
 
     handleCall = (user) => {
@@ -21,15 +22,25 @@ class StreamMenu extends Component{
         this.setState(() => ({
             toStream: true
         }))
-      }
+    }
+
+    handleHome = (user) => {
+        this.setState(() => ({
+            toHome: true
+        }))
+    }
 
     render(){
         if(this.state.toStream === true){
-            return <Redirect to='/StreamFunc/1'/>
+            return <Redirect push to='/StreamFunc/1'/>
         }
 
         if(this.state.toCall === true){
-            return <Redirect to='/StreamFunc/2'/>
+            return <Redirect push to='/StreamFunc/2'/>
+        }
+
+        if(this.state.toHome === true){
+            return <Redirect push to='/Home'/>
         }
 
         return(
@@ -37,7 +48,7 @@ class StreamMenu extends Component{
             <div className={'Home'}>
                            
                 <div className={'menu'}>
-                    <button className={'backButton notBackground'} onClick={() => this.props.history.go(-1)}><FaAngleDoubleLeft size='4em'/></button>
+                    <button className={'backButton notBackground'} onClick={() => this.handleHome()}><FaHome size='4em'/></button>
                     <div className={'menuText notBackground'}>Stream</div>                 
                     <div className={'buttonContainer'}>
                         <button className={'roundButton notBackground'} onClick={this.handleCall}><FaPhone size='4em'/></button>
