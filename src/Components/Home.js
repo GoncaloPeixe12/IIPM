e18e629c1fe045e2e3773e60ce6239899e163199
Duarte Matias <dsm@localhost.localdomain>
2019-05-22 11:57:43 +0100

    Added help button

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,10 +2,26 @@ import React, { Component } from "react";
 import Hub from "./Hub";
 import "bootstrap/dist/css/bootstrap.css";
 import "../style/Home.css";
-import { FaBatteryFull } from "react-icons/fa";
+import { FaBatteryFull, FaQuestionCircle } from "react-icons/fa";
+import {Redirect} from 'react-router-dom';
 
 class Home extends Component {
+
+  state = {
+    toHelp: false
+  }
+
+  handleHelp = (user) => {
+    this.setState(() => ({
+      toHelp: true
+    }))
+  }
+
   render() {
+    if(this.state.toHelp === true){
+      return <Redirect push to='/Home/Help'/>
+    }
+
     return (
       <div className={"Home"}>
         <div className={"container"}>
@@ -15,6 +31,9 @@ class Home extends Component {
               size="3em"
               style={{ right: "1%", top: "1%", position: "absolute" }}
             />
+            <button className={"ui button icon"} style={{ left: "0", top: "0", position: "absolute"}} onClick={()=>this.handleHelp()}>
+              <FaQuestionCircle size="3em"/>
+            </button>
           </div>
         </div>
         <Hub />
