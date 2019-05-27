0b3b48307c13dd5aa562f6893f9f491dd66dfe86
Gongas <goncalo@bash.pt>
2019-05-27 16:35:09 +0100

    updates

--- src/Components/PastRoutes.js
+++ src/Components/PastRoutes.js
@@ -1,40 +1,56 @@
-import React, {Component} from 'react';
-import '../style/App.css';
-import './RoutingOptions';
+import React, { Component } from "react";
+import "../style/App.css";
+import "./RoutingOptions";
 import RoutingOptions from "./RoutingOptions";
- class PastRoutes extends Component {
+class PastRoutes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false,
+      tabId: ""
+    };
+    this.handleHover = this.handleHover.bind(this);
+  }
 
-     constructor(props) {
-         super(props);
-         this.state = {
-             hover: false,
-             tabId: ''
-         }
-         this.handleHover = this.handleHover.bind(this);
-     }
-
-     handleHover = (id) => {
-         this.setState({
-             hover: !this.state.hover,
-             tabId: id
-         })
-     }
-    render() {
-        return(
-            <div className={'grid-container'}>
-                <div onClick={() => this.handleHover(1)}>
-                    {this.state.tabId === 1 && this.state.hover === true ? <RoutingOptions text="Route: 4/2/2019"/> : "Route: 4/2/2019"}
-                </div>
-                <div onClick={() => this.handleHover(2)}>
-                    {this.state.tabId === 2 && this.state.hover === true ? <RoutingOptions text="Route: 17/2/2019"/> : "Route: 17/2/2019"}
-                </div>
-                <div onClick={() => this.handleHover(3)}>
-                    {this.state.tabId === 3 && this.state.hover === true ? <RoutingOptions text="Route: 5/3/2019"/> : "Route: 5/3/2019"}
-                </div>
-                <div onClick={() => this.handleHover(4)}>
-                    {this.state.tabId === 4 && this.state.hover === true ? <RoutingOptions text="Route: 20/3/2019"/> : "Route: 20/3/2019"}
-                </div>
-            </div>
-        )
- }}
- export default PastRoutes;
+  handleHover = id => {
+    this.setState({
+      hover: !this.state.hover,
+      tabId: id
+    });
+  };
+  render() {
+    return (
+      <div className={"grid-container"}>
+        <div onClick={() => this.handleHover(1)}>
+          {this.state.tabId === 1 && this.state.hover === true ? (
+            <RoutingOptions text="Route: 4/2/2019" />
+          ) : (
+            "Route: 4/2/2019"
+          )}
+        </div>
+        <div onClick={() => this.handleHover(2)}>
+          {this.state.tabId === 2 && this.state.hover === true ? (
+            <RoutingOptions text="Route: 17/2/2019" />
+          ) : (
+            "Route: 17/2/2019"
+          )}
+        </div>
+        <div onClick={() => this.handleHover(3)}>
+          {this.state.tabId === 3 && this.state.hover === true ? (
+            <RoutingOptions text="Route: 5/3/2019" />
+          ) : (
+            "Route: 5/3/2019"
+          )}
+        </div>
+        <div onClick={() => this.handleHover(4)}>
+          {this.state.tabId === 4 && this.state.hover === true ? (
+            <RoutingOptions text="Route: 20/3/2019" />
+          ) : (
+            "Route: 20/3/2019"
+          )}
+        </div>
+      </div>
+    );
+  }
+}
+export default PastRoutes;
--- src/Components/RoutingOptions.js
+++ src/Components/RoutingOptions.js
@@ -1,18 +1,16 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router, Link, Route} from "react-router-dom";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
 class RoutingOptions extends Component {
-
-   
-    render() {
-        return(
-            <div className="grid-container">
-                <div>{this.props.text}</div>
-                <Link to={'1/2'}>Review</Link>
-                <div>Rename</div>
-                <div>Delete</div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="grid-container">
+        <div>{this.props.text}</div>
+        <Link to={"1/2"}>Review</Link>
+        <div>Rename</div>
+        <div>Delete</div>
+      </div>
+    );
+  }
 }
 export default RoutingOptions;
